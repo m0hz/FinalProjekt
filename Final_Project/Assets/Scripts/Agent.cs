b0e95434be58726eb6989b66c5bc86b1f809b460
Gary <garypalacios88@gmail.com>
2020-02-11 22:53:48 -0800

    script for enemy

---
+++ Final_Project/Assets/Scripts/Agent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    NavMeshAgent navMeshAgent;
+    int index;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        navMeshAgent.destination = waypoints[0].position;
+
+        index = 0;
+    }
+
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < Mathf.Epsilon)
+        {
+            index++;
+
+            if (index > waypoints.Length - 1)
+                index = 0;
+
+            navMeshAgent.destination = waypoints[index].position;
+        }
+    }
+}
---
+++ Final_Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    public float fireDelay = 1f;
+    public float attackDistance = 5f;
+    public UnityEvent OnFire;
+
+    private float lastFired;
+    private GameObject player;
+    private GameObject target;
+    private NavMeshAgent navMeshAgent;
+    private int wavepointIndex = 0;
+    private Transform targets;
+
+    public void Fire()
+    {
+        lastFired = Time.time;
+        OnFire.Invoke();
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            if (Time.time > lastFired + fireDelay)
+            {
+                Fire();
+            }
+
+            if (navMeshAgent.destination != target.transform.position)
+                navMeshAgent.destination = target.transform.position;
+
+            if (Vector3.Distance(target.transform.position, transform.position) > attackDistance)
+            {
+                target = null;
+                GetComponent<Agent>().enabled = true;
+            }
+
+        }
+        else
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) < attackDistance)
+            {
+                target = player;
+
+                GetComponent<Agent>().enabled = false;
+
+                navMeshAgent.destination = target.transform.position;
+            }
+        }
+    }
+
+}
