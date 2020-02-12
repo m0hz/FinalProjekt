5ab2efb2c2f42e0febd748268dd4002b977f96f3
paul <kevinmond@ymail.com>
2020-02-12 07:39:08 -0800

    nu stuff nu scenes nu sounds

---
+++ Final_Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+    public void UnPause()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+}
---
+++ Final_Project/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+[RequireComponent(typeof(Button))]
+
+public class LoadScene : MonoBehaviour
+{
+
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+
+        button.onClick.AddListener(SwitchScene);
+    }
+
+    void SwitchScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
