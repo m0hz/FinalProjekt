d3cc3c5d406ed2071555acd86ec0800d85660a29
gary <garypalacios88@gmail.com>
2020-02-12 09:56:09 -0800

    Door stuff

---
+++ Final_Project/Assets/Scripts/Trigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject Door = null;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Door.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag =="Player")
+        {
+            Door.SetActive(false);
+        }
+    }
+}
