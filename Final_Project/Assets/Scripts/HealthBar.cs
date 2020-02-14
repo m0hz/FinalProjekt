edb769fd500683e193eca14fc37fd34efef4da2b
Gary <garypalacios88@gmail.com>
2020-02-14 00:07:28 -0800

    i dont even know anymore

---
+++ Final_Project/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBars : MonoBehaviour
+{
+    public PlayerHealth health;
+
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+
+        health.OnHealthChanged.AddListener(UpdateHealthBar);
+    }
+
+    void UpdateHealthBar()
+    {
+        image.fillAmount = health.NormalizedValue;
+    }
+}
