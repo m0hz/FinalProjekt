cef83bc57ef5ba143b8cd57b4c9c080f4e798c32
Gary <garypalacios88@gmail.com>
2020-02-13 23:11:41 -0800

    guns

---
+++ Final_Project/Assets/Scripts/MoveGun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGun : MonoBehaviour
+{
+    public GameObject prefabKey;
+    public Transform parent;
+    private void OnDestroy()
+    {
+        GameObject key = Instantiate(prefabKey);
+        key.transform.SetParent(parent);
+
+        key.gameObject.SetActive(true);
+
+        key.transform.localRotation = Quaternion.identity;
+        key.transform.localPosition = Vector3.zero;
+
+    }
+}
---
+++ Final_Project/Assets/Scripts/WeaponController2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WeaponController2 : MonoBehaviour
+{
+    public UnityEvent OnFire;
+    public float fireRate = 15f;
+    private float nextTimeToFire = 0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            OnFire.Invoke();
+        }
+    }
+}
